7ac9c4988acc94814c434de2ab569b6eba9b28d4
ows3090 <ows3090@naver.com>
2021-03-09 22:45:42 +0900

    Design activity_login.xml

---
+++ app/src/main/java/com/androidstudy/MainActivity.java
@@ -0,0 +1,15 @@
+package com.androidstudy;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@NonNull Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
